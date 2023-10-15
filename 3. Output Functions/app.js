8f0ae7ec5d7e28be74c15c159530fdd5d5e7f753
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-10-16 00:21:05 +0200

    Output functions

---
+++ 3. Output Functions/app.js
@@ -0,0 +1,45 @@
+// Yorum satırı
+
+// ? başka bir tane yorum satırı
+// ! başka bir tane daha
+// * farklı bir renkte daha
+
+
+// document.write("Hello World! <br>");
+// document.write("Şafak Kaya");
+
+
+console.log("Hello World!");
+console.log(25);
+console.log(3.42);
+console.log({ name: "safak", surname: "kaya" });
+
+console.clear();
+
+let: a = 5;
+let: b = 10;
+
+console.log(a + b);
+console.log(a * b);
+
+console.log(
+    "iki sayının toplamı: ", (a + b));
+
+console.clear();
+
+// ? Alert metodu nasıl yapılır?
+
+console.log(window);
+
+console.log(document.location.host);
+
+alert("Merhaba");
+
+alert(a * b);
+
+alert("iki sayının çarpımı: " + (a * b));
+
+
+
+
+
