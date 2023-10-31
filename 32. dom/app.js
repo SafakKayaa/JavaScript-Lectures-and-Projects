49a818b9a2f764c12945cd26af678f2f7d36c0a9
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-11-01 00:41:29 +0100

    styling and creating html elements via js

--- 32. dom/app.js
+++ 32. dom/app.js
@@ -83,5 +83,60 @@ value = todo1.previousElementSibling;
 let baslik = row.children[0].children[3].children[0];
 baslik.textContent="değişmiş H5";
 
+console.clear();
 
+const link = document.createElement("a");
+link.id = "goToMyBlogPage";
+link.className = "btn btn-primary mt-3 btn-sm btn-dark";
+link.innerHTML = "kişisel websiteme git";
+link.style.color = "white";
+
+
+const cardBody = document.querySelectorAll(".card-body")[1];
+
+cardBody.appendChild(link);
+
+
+
+{/* <li class="list-group-item d-flex justify-content-between">
+                Todo 3
+                <a href="#" class="delete-item">
+                  <i class="fa fa-remove"></i>
+                </a>
+              </li> */}
+
+
+const li = document.createElement("li");
+li.innerHTML = "todo 5";
+li.style.listStyle = "none";
+li.style.background = "turquoise"
+li.style.color = "white";
+li.className = "list-group-item d-flex justify-content-between "
+
+const a = document.createElement("a");
+a.className = "delete-item"
+li.appendChild(a);
+
+const i = document.createElement("i");
+i.className = "fa fa-remove";
+a.appendChild(i);
+
+
+
+// console.log(a);
+
+const ul = document.querySelector("ul");
+ul.appendChild(li);
+
+// ul.removeChild(li);
+
+// ul.removeChild(ul.firstElementChild.nextElementSibling)
+
+
+const newTitle = document.createElement("h2");
+newTitle.textContent = "Yeni Başlık"
+
+const oldTitle = cardBody.children[0];
+
+cardBody.replaceChild(newTitle, oldTitle);
 
